58abf6272781754e303d825a37e91ec7d7a9cd7d
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-03-25 17:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,12 +35,14 @@ app.use((err, req, res, next) => {
   });
 });
 
- app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
+ if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('/**', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 var registration = require('./registration.js');
